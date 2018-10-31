bc573f9b4fd86ab6c673d5bd732e01b140bc7103
Tim K <timprogrammer@rambler.ru>
2018-10-31 23:56:20 +0300

    download-3rdparty-components.sh now uses git to fetch GitHub repos

--- src/3rdparty/download-3rdparty-components.sh
+++ src/3rdparty/download-3rdparty-components.sh
@@ -20,7 +20,7 @@ else
 	exit 1
 fi
 
-COMPONENTS="ev3api=https://github.com/c4ev3/EV3-API/archive/master.zip"
+COMPONENTS="ev3api=https://github.com/c4ev3/EV3-API"
 if test -e "./.stamp"; then
 	echo "[INFO] Everything was already downloaded"
 	exit 0
@@ -32,30 +32,44 @@ for Component in $COMPONENTS; do
 	STAMPNAME="$STAMPNAME/.stamp_$CNAME"
 	echo "[INFO] Downloading component \"$CNAME\" from \"$DLURL\""
 	CNAME=`basename "$DLURL"`
-	if $DOWNLOAD_CMD "$CNAME" "$DLURL"; then
-		echo "[INFO] Download finished, trying to unpack it"
-	else
-		echo "[ERROR] Download failed!"
-		exit 2
-	fi
-	if test `echo "$CNAME" | cut -d '.' -f2` = "zip"; then
-		if command -v bsdtar > /dev/null 2>&1; then
-			bsdtar -xvf "$CNAME"
-		elif command -v unzip > /dev/null 2>&1; then
-			unzip "$CNAME"
-		elif command -v 7z > /dev/null 2>&1; then
-			7z x "$CNAME"
+	if echo "$DLURL" | grep -q "github.com"; then
+		if command -v git > /dev/null 2>&1; then; then
+			if git clone "$DLURL" && mv -v `basename "$DLURL"` `basename "$DLURL"`-master/; then
+				echo "[INFO] git clone finished successfully"
+			else
+				echo "[ERROR] Download failed!"
+				exit 2
+			fi
 		else
-			echo "[ERROR] No unpacking utils for archive type \"zip\" were found."
-			exit 3
+			echo "[ERROR] Git is not installed."
+			exit 5
 		fi
-	elif echo "$CNAME" | grep -q ".tar"; then
-		tar -xvf "$CNAME" || tar -xvjf "$CNAME" || tar -xvzf "$CNAME" || tar -xvJf "$CNAME" || bsdtar -xvf "$CNAME" || exit 5
 	else
-		echo "[ERROR] Internal script error"
-		exit 4
-	fi
-	rm -r -f "$CNAME"
+		if $DOWNLOAD_CMD "$CNAME" "$DLURL"; then
+			echo "[INFO] Download finished, trying to unpack it"
+		else
+			echo "[ERROR] Download failed!"
+			exit 2
+		fi
+		if test `echo "$CNAME" | cut -d '.' -f2` = "zip"; then
+			if command -v bsdtar > /dev/null 2>&1; then
+				bsdtar -xvf "$CNAME"
+			elif command -v unzip > /dev/null 2>&1; then
+				unzip "$CNAME"
+			elif command -v 7z > /dev/null 2>&1; then
+				7z x "$CNAME"
+			else
+				echo "[ERROR] No unpacking utils for archive type \"zip\" were found."
+				exit 3
+			fi
+		elif echo "$CNAME" | grep -q ".tar"; then
+			tar -xvf "$CNAME" || tar -xvjf "$CNAME" || tar -xvzf "$CNAME" || tar -xvJf "$CNAME" || bsdtar -xvf "$CNAME" || exit 5
+		else
+			echo "[ERROR] Internal script error"
+			exit 4
+		fi 
+		rm -r -f "$CNAME" 
+	fi 
 	touch "$STAMPNAME"
 done
 echo "[INFO] Done"
