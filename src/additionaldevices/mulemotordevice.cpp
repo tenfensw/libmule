e35b69cce3f182628af8f90701b4eafbcefe2bf0
Tim K <timprogrammer@rambler.ru>
2018-11-04 14:33:07 +0300

    Jamfile now supports minimal libMule builds, MuleMotorDevice prototype finished

---
+++ src/additionaldevices/mulemotordevice.cpp
@@ -0,0 +1,17 @@
+#include "additionaldevices/mulemotordevice.h"
+
+MuleMotorDevice::MuleMotorDevice(int devpin) : MuleDevice(devpin) {
+	muledebug("Initialized MuleMotorDevice with pin " + muleinttostr(devpin));
+}
+
+bool MuleMotorDevice::goBack(int speed) {
+	return this->goForward(0 - speed);
+}
+
+bool MuleMotorDevice::goForward(int speed) {
+	return this->write(speed);
+}
+
+void MuleMotorDevice::stop() {
+	this->goForward(MULE_OTHER_MOTORSTOPCODE);
+}
--- src/additionaldevices/mulemotordevice.h
+++ src/additionaldevices/mulemotordevice.h
@@ -2,11 +2,13 @@
 #define MULEMOTORDEVICE_H
 
 #include "core/muledevice.h"
+#include "core/muleglobalfunctions.h"
 
 class MuleMotorDevice : public MuleDevice {
 	public:
 	  MuleMotorDevice(int devpin);
-	  bool go(int speed);
+	  bool goForward(int speed);
+	  bool goBack(int speed);
 	  void stop();
 	  	
 };
