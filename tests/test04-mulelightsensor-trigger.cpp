4c619246b07eb823bd117e2e786c7f63a178d3bf
Tim K <timprogrammer@rambler.ru>
2018-11-24 00:32:45 +0300

    configure should now install libMule.a in the right place

---
+++ tests/test04-mulelightsensor-trigger.cpp
@@ -0,0 +1,21 @@
+#include <mule.h>
+
+#define SENSORPIN 2
+
+int main() {
+	MuleApplication mApp;
+	muleprintf("MuleLightSensor test on %s", mApp.getPlatformName());
+	muleprintf("Step 1. Create a new instance of MuleLightSensor");
+	MuleLightSensor lsensor(SENSORPIN);
+	muleprintf("success");
+	muleprintf("Step 2. Wait till triggered");
+	if (lsensor.waitUntilTriggered() == false) {
+		muleprintf("failed");
+		mApp.exit(1);
+	}
+	else
+		muleprintf("success");
+	muleprintf("That's it, goodbye");
+	mApp.exit(0);
+	return 0;
+}
