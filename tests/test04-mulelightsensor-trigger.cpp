9bef61ad3e8ff25d8e94779a86fbc4beb0e66f2d
Tim K <timprogrammer@rambler.ru>
2018-11-24 13:30:08 +0300

    Added \n when invoking muleprintf in tests

--- tests/test04-mulelightsensor-trigger.cpp
+++ tests/test04-mulelightsensor-trigger.cpp
@@ -5,17 +5,17 @@
 int main() {
 	MuleApplication mApp;
 	muleprintf("MuleLightSensor test on %s", mApp.getPlatformName().c_str());
-	muleprintf("Step 1. Create a new instance of MuleLightSensor");
+	muleprintf("Step 1. Create a new instance of MuleLightSensor... ");
 	MuleLightSensor lsensor(SENSORPIN);
-	muleprintf("success");
-	muleprintf("Step 2. Wait till triggered");
+	muleprintf("success\n");
+	muleprintf("Step 2. Wait till triggered... ");
 	if (lsensor.waitUntilTriggered() == false) {
-		muleprintf("failed");
+		muleprintf("failed\n");
 		mApp.exit(1);
 	}
 	else
-		muleprintf("success");
-	muleprintf("That's it, goodbye");
+		muleprintf("success\n");
+	muleprintf("That's it, goodbye\n");
 	mApp.exit(0);
 	return 0;
 }
