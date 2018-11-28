69fbf2130c9d58b233f9a59ed5a1c0903731f6bf
Tim K <timprogrammer@rambler.ru>
2018-11-29 00:44:19 +0300

    Added GNU LGPL banner into bash scripts too

--- src/3rdparty/download-3rdparty-components.sh
+++ src/3rdparty/download-3rdparty-components.sh
@@ -1,6 +1,23 @@
-
 #!/bin/bash
-# This will download third-party components
+# download-3rdparty-components.sh - This bash script will download third-party components
+# This file is a part of libMule - Microcontroller-Universal 
+# Library (that is extendable)
+# Copyright (C) 2018 Tim K <timprogrammer@rambler.ru>
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License.
+#  
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
+# 02110-1301  USA
 
 if test "$1" = "clean"; then
 	rm -r -f ./.stamp* ./.stamp
